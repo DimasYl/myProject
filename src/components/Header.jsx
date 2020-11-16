02359c28cbd907c0b874df8a147acc4a2d7e606e
DimasYl <dima.ilyushin.2106@mail.ru>
2020-11-16 10:49:28 +0300

    start

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://sntch.com/uploads/2019/07/mortal_kombat_PNG23.png" />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Profile.css'
+
+const Profile = () => {
+  return (
+    <div className="content ">
+      <img src="https://img3.goodfon.ru/original/1920x1200/2/a1/koshki-zelennye-glaza-zelenaya.jpg" />
+      <div>ava + description</div>
+      <div>
+        My post
+        <div>
+          New post
+          <div>Post1</div>
+          <div>Post2</div>
+          <div>Post3</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
